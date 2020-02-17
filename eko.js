53e5367833c3a90cf564c00237190d2d2d20c214
Dop <pitipat.dop@gmail.com>
2020-02-17 13:05:48 +0700

    ...

--- eko.js
+++ eko.js
@@ -10,11 +10,11 @@ class EkoClientWrapper {
     if (!EkoClientWrapper.allowInstantiation) {
       throw new Error('Please don\'t call new EkoClientWrapper directly, ust EkoClientWrapper.getInstance()');
     } else {
-      const client = new EkoClient({ apiKey: 'b3b9ec583fdef73348328a14555c16db840c85e1ba30662c' });
+      const client = new EkoClient({ apiKey: 'APIKEY' });
       // Register Session with EkoClient with userId and display name
       client.registerSession({
-        userId: 'trueugc-stag-admin',
-        displayName: 'Stage Admin 2',
+        userId: 'USERID',
+        displayName: 'DISPLAYNAME',
       });
       
       return client;
